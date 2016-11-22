924da8264afca3a7c71679faa33d16537ef891ae
Kiryukhin, Vadim <vkiryukhin@conversantmedia.com>
2016-11-21 21:26:14 -0800

    XML beautify is implemented

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import vkbeautify
 def main():
     #name = sys.argv[1]
     #print(vkbeautify.greeting('Vadim    Kir', 4))
-    str = '.headbg{margin:0 8px  /*display:none*/}a:link,a:focus{color:#00c }a:active{color:red }'
-    css = vkbeautify.css(str);
-    print(css)
+    css_str = '.headbg{margin:0 8px  /*display:none*/}a:link,a:focus{color:#00c }a:active{color:red }'
+    #xml_str = '<a><b>bbb</b></a>'
+    xml_str = '<?xml version="1.0" encoding="UTF-8" ?><!DOCTYPE foo SYSTEM "Foo.dtd"><a><b>bbb</b><c/><d><soapenv:Envelope xmlns:soapenv="http://xxx" xmlns:xsd="http://yyy" xmlns:xsi="http://zzz"></soapenv></d><e><![CDATA[ <z></z> ]]></e><f><g></g></f></a>'
+    css = vkbeautify.css(css_str);
+    #print(css)
     cssmin = vkbeautify.cssmin(css, False)
-    print(cssmin)
+    #print(cssmin)
+
+    print(vkbeautify.xml(xml_str))
 
 if __name__ =='__main__':
     main()
--- vkbeautify.py
+++ vkbeautify.py
@@ -23,12 +23,113 @@ def xml(text, step=4):
     ar = re.sub('\s*xmlns\=', "~::~xmlns=", ar)
     ar = ar.split('~::~')
 
-    #len = ar.length,
-    inComment = False,
-    deep = 0,
-    str = '',
-    ix = 0,
+    length = len(ar)
+    inComment = False
+    deep = 0
+    str = ''
+    ix = 0
     shift = createShiftArr(step)
+
+    while ix < length:
+        # start comment or <![CDATA[...]]> or <!DOCTYPE
+        if re.search('<!', ar[ix]):
+            str += shift[deep]+ar[ix]
+            inComment = True
+            # end comment  or <![CDATA[...]]>
+            if re.search('-->', ar[ix]) or re.search('\]>', ar[ix]) or re.search('!DOCTYPE',ar[ix]):
+                inComment = False
+
+        # end comment  or <![CDATA[...]]>
+        elif re.search('-->',ar[ix]) or re.search('\]>',ar[ix]):
+            str += ar[ix]
+            inComment = False
+        # <elm></elm>
+        elif ( re.search(r'^<\w',ar[ix-1]) and
+               re.search(r'^</\w', ar[ix]) and
+               ( re.search('^<[\w:\-\.\,]+',ar[ix-1]).group(0) == re.sub('/','', re.search(r'^</[\w:\-\.\,]+', ar[ix]).group(0)) )
+            ):
+            str += ar[ix]
+            if not inComment:
+                deep -= 1
+
+        # <elm>
+        elif (re.search('<\w',ar[ix]) and not re.search('<\/',ar[ix]) and not re.search('\/>', ar[ix]) ):
+            if not inComment:
+                str += shift[deep]+ar[ix]
+                deep += 1
+            else:
+                str += ar[ix]
+         # <elm>...</elm>
+        elif re.search('<\w', ar[ix]) and re.search(r'</',ar[ix]):
+            if not inComment:
+                str += shift[deep]+ar[ix]
+            else:
+                str += ar[ix]
+        # </elm>
+        elif re.search(r'</', ar[ix]):
+            #print(ar[ix])
+            if not inComment:
+                deep -= 1
+                str += shift[deep]+ar[ix]
+            else:
+                str += ar[ix]
+        # <elm/>
+        elif re.search('\/>', ar[ix]):
+            if not inComment:
+                str += shift[deep]+ar[ix]
+            else:
+                str += ar[ix]
+        # <? xml ... ?>
+        elif re.search('<\?', ar[ix]):
+            str += shift[deep]+ar[ix]
+        # xmlns
+        elif re.search('xmlns\:', ar[ix]) or re.search('xmlns\=',ar[ix]):
+            str += shift[deep]+ar[ix];
+        else:
+            str += ar[ix];
+
+
+        ix += 1
+
+
+    return str
+'''
+        else
+         // <elm> //
+        if(ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) == -1 && ar[ix].search(/\/>/) == -1 ) {
+            str = !inComment ? str += shift[deep++]+ar[ix] : str += ar[ix];
+        } else
+         // <elm>...</elm> //
+        if(ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) > -1) {
+            str = !inComment ? str += shift[deep]+ar[ix] : str += ar[ix];
+        } else
+        // </elm> //
+        if(ar[ix].search(/<\//) > -1) {
+            str = !inComment ? str += shift[--deep]+ar[ix] : str += ar[ix];
+        } else
+        // <elm/> //
+        if(ar[ix].search(/\/>/) > -1 ) {
+            str = !inComment ? str += shift[deep]+ar[ix] : str += ar[ix];
+        } else
+        // <? xml ... ?> //
+        if(ar[ix].search(/<\?/) > -1) {
+            str += shift[deep]+ar[ix];
+        } else
+        // xmlns //
+        if( ar[ix].search(/xmlns\:/) > -1  || ar[ix].search(/xmlns\=/) > -1) {
+            str += shift[deep]+ar[ix];
+        }
+
+        else {
+            str += ar[ix];
+        }
+    }
+
+    return  (str[0] == '\n') ? str.slice(1) : str;
+'''
+        ##ix += 1
+
+
 
 #
 # Beautify CSS
@@ -46,7 +147,6 @@ def css(text, step=4):
 
     deep = 0
     str = ''
-    ix = 0
     shift = createShiftArr(step)
 
     for item in ar:
@@ -62,23 +162,6 @@ def css(text, step=4):
             str += shift[deep]+item
 
     return str
-'''
-    while ix<len(ar):
-        if re.search('\{', ar[ix]):
-            str += shift[deep]+ar[ix]
-            deep = deep+1
-        elif re.search('\}', ar[ix]):
-            deep = deep-1
-            str += shift[deep]+ar[ix]
-        elif re.search('\*\\\\', ar[ix]):
-            str += shift[deep]+ar[ix]
-        else:
-            str += shift[deep]+ar[ix]
-
-        ix += 1
-'''
-    #return str
-
 
 #
 # Minify CSS
